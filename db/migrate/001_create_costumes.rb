32569784b759b3128a75ef7160289cee3ba3c518
J K <jkim65@buffalo.edu>
2020-10-12 23:56:17 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,10 @@ class Costume < ActiveRecord::Migration[5.1]
 
   def change
     create_table :costumes do |t|
-      
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.string :image_url
   end
 end
 
