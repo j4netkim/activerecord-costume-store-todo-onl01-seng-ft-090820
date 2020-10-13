1cb28888d4b1250d9f098de36ab0ceb6c68672e9
J K <jkim65@buffalo.edu>
2020-10-13 00:50:33 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,7 +4,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.string :theme
+      t.boolean :theme
       t.float :price
       t.boolean :family_friendly
       t.datetime :opening_date
