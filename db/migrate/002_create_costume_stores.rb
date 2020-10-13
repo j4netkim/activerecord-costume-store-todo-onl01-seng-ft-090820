60a4860b72286d981677ed1307461082810340ed
J K <jkim65@buffalo.edu>
2020-10-13 00:37:40 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,6 +8,6 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.
+      t.integer :number_of_employees
   end
 end
