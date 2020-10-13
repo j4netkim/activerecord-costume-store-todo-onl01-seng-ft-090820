fc3a4bacbd939604eaa4f19315340144dc85c630
J K <jkim65@buffalo.edu>
2020-10-13 00:31:25 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,6 +4,10 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
 
   def change
-    create_table :costume_stores
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      
   end
 end
